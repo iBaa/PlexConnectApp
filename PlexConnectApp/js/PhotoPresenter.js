d4466e47fc72e3f7d17a5a3c2aa46a70931e0669
iBaa <bernhard.baaser@gmx.de>
2015-11-13 23:39:37 +0100

    very basic photo slideshow (js)

--- PlexConnectApp/js/PhotoPresenter.js
+++ PlexConnectApp/js/PhotoPresenter.js
@@ -31,6 +31,51 @@ show: function(pmsId, pmsPath) {
   navigationDocument.pushDocument(doc);
 },
 
+slideshow: function(pmsId, pmsPath) {
+  // get photo list
+  var docString = swiftInterface.getViewIdPath('Photos', pmsId, pmsPath);  // error handling?
+  
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(docString, "application/xml");
+
+  var playlist = doc.getElementByTagName("playlist")
+  var photoDoc = undefined  // no photo on stack yet
+  
+  // start slideshow
+  photoPresenter.showNext(playlist, photoDoc);
+},
+
+showNext: function(playlist, photoDoc) {
+  // exit if not in control of the screen
+  var doc = navigationDocument.documents[navigationDocument.documents.length-1];
+  if (photoDoc && photoDoc != doc) {
+    return
+  }
+  
+  // next photo
+  var photo = playlist.getElementByTagName("photo");
+  // end slideshow, exit if no photo left
+  if (photo==undefined) {
+    navigationDocument.popDocument();
+    return
+  }
+  
+  // remove photo from playlist
+  playlist.removeChild(photo);
+  
+  // render picture to screen
+  var nextPhotoDoc = createPhoto(photo.textContent.tagsToHtmlEntities());
+  if (!photoDoc) {
+    // first photo
+    navigationDocument.pushDocument(nextPhotoDoc);
+  } else {
+    // later photos - replace
+    navigationDocument.replaceDocument(nextPhotoDoc, photoDoc);
+  }
+
+  // wait some time, request next one...
+  setTimeout( function() { photoPresenter.showNext(playlist, nextPhotoDoc) }, 3000);  // todo: waiting period in settings
+},
 }
 
 
